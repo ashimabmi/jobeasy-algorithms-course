bd00a3270a55bdb0e033561ec8ca842912d7392f
ashimabmi <ashimabmi@gmail.com>
2021-02-03 20:23:01 -0800

    modified hw1

--- lesson1_challenge.py
+++ lesson1_challenge.py
@@ -5,8 +5,5 @@ driver.delete_all_cookies()
 driver.maximize_window()
 driver.implicitly_wait(5)
 driver.get("https://www.amazon.com/s?k=lego")
-lego_li = driver.find_elements(By.CSS_SELECTOR,
-"div[class='s-main-slot s-result-list s-search-results sg-row'] span[data-component-type='s-product-image'] img ")
-lego_first = lego_li[0]
-print(lego_first.get_attribute("alt"))
-lego_first.click()
+lego_li_first = driver.find_element(By.CSS_SELECTOR, "div[class='s-main-slot s-result-list s-search-results sg-row']>div:nth-child(2) div>a")
+lego_li_first.click()
